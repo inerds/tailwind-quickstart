ba5656b5b28262935d9a0fa85b333ae9f20dadcc
Hussain Inad <nerdsane@gmail.com>
2020-08-23 14:43:09 +0500

    added purgecss and cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
 	plugins: [
-	  require('tailwindcss'),
-	  require('autoprefixer')
-	]
+		require('tailwindcss'),
+		require('autoprefixer'),
+		cssnano({
+		  preset: 'default'
+		}),
+		purgecss({
+		  content: ['./*.html'],
+		  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+		})
+	  ]
 }
